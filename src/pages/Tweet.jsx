4c90d1af44b83d6071cc6239cce4a596589f3020
DireWolf707 <rahulsood707@gmail.com>
2023-05-22 12:43:36 +0530

    added tweet page

--- src/pages/Tweet.jsx
+++ src/pages/Tweet.jsx
@@ -1,7 +1,30 @@
-import { Stack } from "@mui/material"
+import { useParams } from "react-router"
+import TwitterContainer from "../components/twitter/TwitterContainer"
+import TweetCard from "../components/twitter/TweetCard"
+import TweetInput from "../components/twitter/TweetInput"
+
+const t = {
+  id: 123,
+  name: "Direwolf",
+  username: "direwolf",
+  time: "2 hours",
+  content: "This is my first tweet",
+  userId: 1234
+}
 
 const Tweet = () => {
-  return <Stack flexGrow={1}>Feed</Stack>
+  const { tweetId } = useParams()
+
+  return (
+    <TwitterContainer heading="tweet">
+      <TweetCard tweet={t} />
+
+      <TweetInput parentTweetId={tweetId} />
+
+      <TweetCard tweet={t} />
+      <TweetCard tweet={t} />
+    </TwitterContainer>
+  )
 }
 
 export default Tweet
