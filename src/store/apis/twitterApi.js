1d40de0a74f5263943afb5bbef8d598834e12340
DireWolf707 <rahulsood707@gmail.com>
2023-05-23 01:41:15 +0530

    changed reducerPath

--- src/store/apis/twitterApi.js
+++ src/store/apis/twitterApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export default createApi({
-  reducerPath: "twitter",
+  reducerPath: "twitterApi",
 
   baseQuery: fetchBaseQuery({
     baseUrl: `${import.meta.env.VITE_SERVER_URL}/twitter`,
--- src/store/apis/userApi.js
+++ src/store/apis/userApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export default createApi({
-  reducerPath: "user",
+  reducerPath: "userApi",
 
   baseQuery: fetchBaseQuery({
     baseUrl: `${import.meta.env.VITE_SERVER_URL}/user`,
