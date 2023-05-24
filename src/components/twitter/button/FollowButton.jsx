d6e814ee12bec86ed0b1b6ff2bf9ec09fc6ac4e3
DireWolf707 <rahulsood707@gmail.com>
2023-05-25 00:48:34 +0530

    added ogic

--- src/components/twitter/button/FollowButton.jsx
+++ src/components/twitter/button/FollowButton.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react"
 import { Button } from "@mui/material"
+import { twitterApi } from "../../../store"
+import requestHandler from "../../../utils/requestHandler"
 
-const FollowButton = ({ isFollowed }) => {
+const FollowButton = ({ userId, isFollowed: _isFollowed }) => {
+  const [isFollowed, setIsFollowed] = useState(_isFollowed)
   const [isUnfollowBtnHovered, setisUnfollowBtnHovered] = useState(false)
+  const [follow, { isLoading: isFollowing }] = twitterApi.useFollowMutation()
+  const [unfollow, { isLoading: isUnfollowing }] = twitterApi.useUnfollowMutation()
+
+  const followHandler = () => requestHandler(follow({ userId }).unwrap(), "following", "followed").then(() => setIsFollowed(true))
+
+  const unfollowHandler = () => requestHandler(unfollow({ userId }).unwrap(), "unfollowing", "unfollowed").then(() => setIsFollowed(false))
 
   if (isFollowed)
     return (
       <Button
+        onClick={unfollowHandler}
+        disabled={isUnfollowing}
         onMouseEnter={() => setisUnfollowBtnHovered(true)}
         onMouseLeave={() => setisUnfollowBtnHovered(false)}
         variant="contained"
@@ -15,7 +26,7 @@ const FollowButton = ({ isFollowed }) => {
           border: isUnfollowBtnHovered ? "2px solid red" : "2px solid rgba(150,150,150)",
           borderRadius: "24px",
           textTransform: "capitalize",
-          py: "4px"
+          py: "4px",
         }}
       >
         {isUnfollowBtnHovered ? "unfollow" : "following"}
@@ -23,7 +34,13 @@ const FollowButton = ({ isFollowed }) => {
     )
 
   return (
-    <Button variant="contained" color="btn2" sx={{ borderRadius: "24px", textTransform: "capitalize", py: "4px" }}>
+    <Button
+      onClick={followHandler}
+      disabled={isFollowing}
+      variant="contained"
+      color="btn2"
+      sx={{ borderRadius: "24px", textTransform: "capitalize", py: "4px" }}
+    >
       follow
     </Button>
   )
