93fc76c44b77b1699c57059a51062e4bf883d251
DireWolf707 <rahulsood707@gmail.com>
2023-05-25 02:48:50 +0530

    added recomendation api

---
+++ src/components/twitter/card/FollowRecomendationCard.jsx
@@ -0,0 +1,25 @@
+import { Stack, Typography } from "@mui/material"
+import UserAvatar from "../../layout/UserAvatar"
+import { Link } from "react-router-dom"
+
+const FollowRecomendationCard = ({ user }) => {
+  return (
+    <Stack flexDirection="row" alignItems="center" gap={2}>
+      <UserAvatar user={user} />
+
+      <Stack>
+        <Link to={`/public-profile/${user.userId}`}>
+          <Typography fontWeight={600} fontSize="14px">
+            {user.name}
+          </Typography>
+        </Link>
+
+        <Typography fontWeight={600} fontSize="13px" color="rgba(150,150,150)">
+          @{user.username}
+        </Typography>
+      </Stack>
+    </Stack>
+  )
+}
+
+export default FollowRecomendationCard
--- src/components/twitter/sidebar/Rightbar.jsx
+++ src/components/twitter/sidebar/Rightbar.jsx
@@ -1,10 +1,12 @@
 import { useLocation } from "react-router-dom"
+import { twitterApi } from "../../../store"
 import { Stack, Typography } from "@mui/material"
-import UserAvatar from "../../layout/UserAvatar"
+import FollowRecomendationCard from "../card/FollowRecomendationCard"
 import SearchButton from "../button/SearchButton"
 
 const Rightbar = () => {
   const { pathname } = useLocation()
+  const { data, isFetching, isError } = twitterApi.useGetFollowRecomendationsQuery()
 
   return (
     <Stack flexShrink={0} width="250px" gap={2} sx={{ display: { xs: "none", lg: "flex" } }}>
@@ -15,19 +17,9 @@ const Rightbar = () => {
           Who to follow
         </Typography>
 
-        <Stack flexDirection="row" alignItems="center" gap={2}>
-          <UserAvatar user={{ username: "DW" }} />
+        <FollowRecomendationCard user={{ name: "Direwolf", username: "direwolf707", userId: "11abdbb8-0a5d-492e-b757-e6e0844a9474" }} />
 
-          <Stack>
-            <Typography fontWeight={600} fontSize="14px">
-              Direwolf
-            </Typography>
-
-            <Typography fontWeight={600} fontSize="13px" color="rgba(150,150,150)">
-              @direwolf
-            </Typography>
-          </Stack>
-        </Stack>
+        <>{isFetching || isError ? <>loading</> : data.data.map((recomendation) => <FollowRecomendationCard user={recomendation} />)}</>
       </Stack>
     </Stack>
   )
