a7e0e6c023d07f2c97bfc3b0b1bd33fc7d1ce527
DireWolf707 <rahulsood707@gmail.com>
2023-05-25 13:54:56 +0530

    added get tweet and comment api

--- src/pages/Tweet.jsx
+++ src/pages/Tweet.jsx
@@ -1,20 +1,34 @@
+import { useState, useEffect, useCallback } from "react"
 import { useParams } from "react-router"
 import { twitterApi } from "../store"
 import TwitterContainer from "../components/twitter/TwitterContainer"
 import TweetCard from "../components/twitter/card/TweetCard"
 import TweetInput from "../components/twitter/TweetInput"
+import requestHandler from "../utils/requestHandler"
 
 const Tweet = () => {
   const { tweetId } = useParams()
-  const { data, isFetching, isError, refetch } = twitterApi.useGetTweetQuery({ tweetId })
+  const [tweet, setTweet] = useState(null)
+  const [comments, setComments] = useState(null)
+  const [getTweet] = twitterApi.useLazyGetTweetQuery()
+  const [getComments] = twitterApi.useLazyGetCommentsQuery()
+
+  const refetch = useCallback(async () => {
+    await requestHandler(getComments({ tweetId }).unwrap(), "fetching comments", "comments fetched").then(({ data }) => setComments(data))
+  }, [])
+
+  useEffect(() => {
+    requestHandler(getTweet({ tweetId }).unwrap(), "fetching tweet", "tweet fetched").then(({ data }) => setTweet(data))
+    refetch()
+  }, [])
 
   return (
     <TwitterContainer heading="tweet" refetch={refetch}>
-      <>{isFetching || isError ? <>loading</> : <TweetCard tweet={data.data} />}</>
+      {tweet ? <TweetCard tweet={tweet} /> : <>loading</>}
 
       <TweetInput parentTweetId={tweetId} />
 
-      <>{isFetching || isError ? <>loading</> : data.data.comments.map((comment) => <TweetCard key={comment.id} tweet={comment} />)}</>
+      {comments ? comments.map((comment) => <TweetCard key={comment.id} tweet={comment} />) : <>loading</>}
     </TwitterContainer>
   )
 }
