e449bed7fcf18a5a84d30c7824bb2bd6a0dd0245
DireWolf707 <rahulsood707@gmail.com>
2023-05-26 01:43:31 +0530

    updated with  ParentChildTweetCard

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback } from "react"
 import { twitterApi } from "../store"
 import TwitterContainer from "../components/twitter/TwitterContainer"
+import ParentChildTweetCard from "../components/twitter/card/ParentChildTweetCard"
 import TweetCard from "../components/twitter/card/TweetCard"
 import TweetInput from "../components/twitter/TweetInput"
 import requestHandler from "../utils/requestHandler"
@@ -20,13 +21,24 @@ const Feed = () => {
     refetch()
   }, [])
 
-  const onComplete = ({ data }) => setFeed((pv) => [data, ...pv])
+  const onComplete = ({ data }) => setFeed((pv) => [{ tweet: data }, ...pv])
 
   return (
     <TwitterContainer heading="home" refetch={refetch}>
       <TweetInput onComplete={onComplete} disabled={!Boolean(feed)} />
 
-      {feed ? feed.length ? feed.map((tweet) => <TweetCard key={tweet.id} tweet={tweet} />) : <EmptyCard /> : <CircularLoader />}
+      {feed ? (
+        feed.length ? (
+          feed.map(({ tweet, parentTweet }) => {
+            if (parentTweet) return <ParentChildTweetCard key={tweet.id} parentTweet={parentTweet} tweet={tweet} />
+            return <TweetCard key={tweet.id} tweet={tweet} />
+          })
+        ) : (
+          <EmptyCard />
+        )
+      ) : (
+        <CircularLoader />
+      )}
     </TwitterContainer>
   )
 }
