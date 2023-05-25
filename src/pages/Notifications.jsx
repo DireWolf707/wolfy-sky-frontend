bfd53dee55be9cea7bb7d5fb5cd189a27c13f728
DireWolf707 <rahulsood707@gmail.com>
2023-05-25 15:44:19 +0530

    added logic

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,19 +1,31 @@
+import { useState, useEffect, useCallback } from "react"
 import { twitterApi } from "../store"
 import TwitterContainer from "../components/twitter/TwitterContainer"
 import NotificationCard from "../components/twitter/card/NotificationCard"
+import requestHandler from "../utils/requestHandler"
 
 const Notifications = () => {
-  const { data, isFetching, isError, refetch } = twitterApi.useGetNotificationsQuery()
+  const [notifications, setNotifications] = useState(null)
+  const [getNotifications] = twitterApi.useLazyGetNotificationsQuery()
+
+  const refetch = useCallback(async () => {
+    setNotifications(null)
+    await requestHandler(getNotifications().unwrap(), "fetching notifications", "notifications fetched").then(({ data }) =>
+      setNotifications(data)
+    )
+  }, [])
+
+  useEffect(() => {
+    refetch()
+  }, [])
 
   return (
     <TwitterContainer heading="notifications" refetch={refetch}>
-      <>
-        {isFetching || isError ? (
-          <>loading</>
-        ) : (
-          data.data.map((notification) => <NotificationCard key={notification.id} notification={notification} />)
-        )}
-      </>
+      {notifications ? (
+        notifications.map((notification) => <NotificationCard key={notification.id} notification={notification} />)
+      ) : (
+        <>loading</>
+      )}
     </TwitterContainer>
   )
 }
