a0f52746749bb4d485d5a0adb3f89f5607936a29
DireWolf707 <rahulsood707@gmail.com>
2023-05-25 14:07:07 +0530

    refactored

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,16 +1,27 @@
+import { useState, useEffect, useCallback } from "react"
 import { twitterApi } from "../store"
 import TwitterContainer from "../components/twitter/TwitterContainer"
 import TweetCard from "../components/twitter/card/TweetCard"
 import TweetInput from "../components/twitter/TweetInput"
+import requestHandler from "../utils/requestHandler"
 
 const Feed = () => {
-  const { data, isFetching, isError, refetch } = twitterApi.useGetFeedQuery()
+  const [feed, setFeed] = useState(null)
+  const [getFeed] = twitterApi.useLazyGetFeedQuery()
+
+  const refetch = useCallback(async () => {
+    await requestHandler(getFeed().unwrap(), "fetching feed", "feed fetched").then(({ data }) => setFeed(data))
+  }, [])
+
+  useEffect(() => {
+    refetch()
+  }, [])
 
   return (
     <TwitterContainer heading="home" refetch={refetch}>
       <TweetInput />
 
-      <>{isFetching || isError ? <>loading</> : data.data.map((tweet) => <TweetCard key={tweet.id} tweet={tweet} />)}</>
+      {feed ? feed.map((tweet) => <TweetCard key={tweet.id} tweet={tweet} />) : <>loading</>}
     </TwitterContainer>
   )
 }
