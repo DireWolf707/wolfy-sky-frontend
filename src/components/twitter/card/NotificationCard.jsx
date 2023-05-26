5662619960bbb9fd055a45f04dc202eed85034f8
DireWolf707 <rahulsood707@gmail.com>
2023-05-26 15:48:36 +0530

    added date format

--- src/components/twitter/card/NotificationCard.jsx
+++ src/components/twitter/card/NotificationCard.jsx
@@ -1,6 +1,7 @@
 import { Stack, Typography } from "@mui/material"
 import { Link } from "react-router-dom"
 import UserAvatar from "../../layout/UserAvatar"
+import DateFormat from "./DateFormat"
 
 const TweetLink = ({ href }) => (
   <Link to={`/tweet/${href}`} style={{ textDecoration: "underline", textUnderlineOffset: "4px", textDecorationColor: "#4072F4" }}>
@@ -37,16 +38,20 @@ const NotificationCard = ({ notification }) => {
     <Stack flexDirection="row" alignItems="center" p="18px" gap={1.5}>
       <UserAvatar user={notification.user} />
 
-      <Typography fontWeight={500} fontSize="16px">
-        <Link
-          to={`/public-profile/${notification.user.id}`}
-          style={{ textDecoration: "underline", textUnderlineOffset: "4px", textDecorationColor: "#4072F4" }}
-        >
-          {notification.user.name}
-        </Link>
-        &nbsp;
-        <NotificationContent notification={notification} />
-      </Typography>
+      <Stack gap={0.5}>
+        <Typography fontWeight={500} fontSize="16px" >
+          <Link
+            to={`/public-profile/${notification.user.id}`}
+            style={{ textDecoration: "underline", textUnderlineOffset: "4px", textDecorationColor: "#4072F4" }}
+          >
+            {notification.user.name}
+          </Link>
+          &nbsp;
+          <NotificationContent notification={notification} />
+        </Typography>
+
+        <DateFormat date={notification.createdAt} />
+      </Stack>
     </Stack>
   )
 }
